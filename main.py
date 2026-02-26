87495079c452388af9c9e79a1089dd3152f49366
Daniel <89848369+DanielMarcosPires@users.noreply.github.com>
2026-02-25 23:39:48 -0300

    Organizando o terminal

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import os
+import sys
 import openpyxl
 import glob
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 class folder:
     def __init__(self, name:str):
@@ -8,12 +20,12 @@ class folder:
             os.mkdir(f"./{name}")
             print("Folder %s created suscefully! " %f"./{name}")
         except FileExistsError:
-            print("Folder %s already exists " % f"./{name}")
+            print(bcolors.HEADER+ "Folder %s already exists " % f"./{name}")
 
     def generateFolders(self, title, names:list): 
         try: 
             os.mkdir(f'./{title}')
-            print("Folder %s created suscefully! " %f'./{title}')
+            print(bcolors.WARNING+"Folder %s created suscefully! \n" %f'./{title}')
             for name in names:
                 try:
                     os.mkdir(f'./{title}/{name}')
@@ -23,16 +35,15 @@ class folder:
         except FileExistsError:
             print("Folder %s already exists " % f'./{title}')
 
-        
 class excel:
-    def reader(self,folderReader:str) -> dict:
+    def reader(self,folderReader:str):
         listNames = []
 
         arquivos = glob.glob(f'./{folderReader}/*.xlsx')
         
         if not arquivos:
-            print("Folder's not found!")
-            return # type: ignore
+            print("Folder's in %s not found!"%f'./{folderReader}')
+            return False
 
         folderPath = arquivos[0]
 
@@ -46,23 +57,23 @@ class excel:
 
 
 def main():
-    print("Program Iniciated! \n")
+    print(bcolors.OKBLUE+"Program Iniciated! \n")
     folderReader = "Leitor de Planilha"
 
     fold = folder(folderReader)
     excelSheet = excel().reader(folderReader) # type: ignore
-    
-    title = excelSheet['Title']
-    names = excelSheet['Names']
-    
-    print(title)
-    print(names)
-    fold.generateFolders(title,names) 
+    if excelSheet:
+        title = excelSheet['Title']
+        names = excelSheet['Names']
+       
+        fold.generateFolders(title,names) 
     
 
 
 
 if __name__ == "__main__":
-     print("="*24)
+     print(bcolors.OKBLUE+"="*24)
      main()
-     print("="*24)
+     print(bcolors.OKBLUE+"="*24)
+
+sys.exit()
