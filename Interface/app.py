dbf68a119e4b38776511fc1b3d956a76293b5abc
Daniel <89848369+DanielMarcosPires@users.noreply.github.com>
2026-02-26 15:03:07 -0300

    Implementando o formulário

--- Interface/app.py
+++ Interface/app.py
@@ -1,16 +1,21 @@
 import customtkinter
 from components.form import form
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         self.title("my-app")
         self.geometry("750x750")
-        self.grid_columnconfigure((0,1),weight=1)
+        # Instancia o componente passando 'self' como pai
+        self.meu_form = form(master=self)
+        self.meu_form.pack(expand=True)
+      
 
-
-
-#teste
+    def mostrar_dados(self):
+        dados = self.meu_form.get_values()
+        print(f"Usuário: {dados['User']}")
+        print(f"Senha: {dados['Password']}")
 
 app = App()
 app.mainloop()
--- Interface/components/form.py
+++ Interface/components/form.py
@@ -1,11 +1,39 @@
-import customtkinter
+import customtkinter 
+
+class form(customtkinter.CTkFrame):
+    def __init__(self,master,**kwargs):
+        super().__init__(master,**kwargs)
+        self.inputs = {}
+        self.inputBox(
+            label="User:",
+            entry="User:"
+        )
+        self.inputBox(
+            label="Password:",
+            entry="Password:"
+        )
+        self.button = customtkinter.CTkButton(self,text="Enviar!",command=self.login_action)
+        self.button.pack(pady=20)
 
 
-class form(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-
-        print()
+    def login_action(self):
+        dados = self.get_values()
+        print(f"Tentativa de login com: {dados}")
     
+    #Junção de dois componentes
+    def inputBox(self, label:str,entry:str):
+        self.label(label)
+        self.inputs[label] = self.entry(entry)
 
-    
+    #Componentes
+    def label(self, text:str):
+        ctkLabel = customtkinter.CTkLabel(self,text=text)
+        ctkLabel.pack()
+
+    def entry(self,prompt:str):
+        inputBox = customtkinter.CTkEntry(self, placeholder_text=prompt)
+        inputBox.pack(padx=10,pady=20)
+        return inputBox
+    #Retornos dos valores
+    def get_values(self):
+        return {name: widget.get() for name, widget in self.inputs.items()}
